aa835a521a0c5e3a112ce378aa9f0a0a6694f414
rp-bot_win10 <legacy.business18@gmail.com>
2023-03-29 19:51:23 -0400

    added audio element

---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const AudioPlayer = () => {
+	return (
+		<div className="audio-player">
+			<audio controls />
+		</div>
+	);
+};
+export default AudioPlayer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import AudioPlayer from "@/components/AudioPlayer";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<AudioPlayer />
 		</>
 	);
 }
